fb046eb5a625915c3bfda459e230250e14ca803b
Gordon D <gordon.draine97@yahoo.com>
2023-11-14 22:38:58 -0500

    completing javascript file

--- Rotating_Navigation/index.js
+++ Rotating_Navigation/index.js
@@ -1,2 +1,5 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
