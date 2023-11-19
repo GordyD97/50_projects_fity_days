33595f4508e9f8678596c1a1c8ef1b60d9f52beb
Gordon D <gordon.draine97@yahoo.com>
2023-11-18 23:15:09 -0500

    adding new project

--- Rotating_Navigation/index.js
+++ Rotating_Navigation/index.js
@@ -3,3 +3,4 @@ const close = document.getElementById('close')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
