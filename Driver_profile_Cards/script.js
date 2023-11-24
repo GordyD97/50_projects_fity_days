2d0707aba1ec7c09b6e7791b734a60e5b5fdfe91
Gordon D <gordon.draine97@yahoo.com>
2023-11-23 21:09:37 -0500

    fixing javascript file for correct rendering of data

--- Driver_profile_Cards/script.js
+++ Driver_profile_Cards/script.js
@@ -1,12 +1,32 @@
-stats = [ driver stats]
-stats = 
-[
-{stats.wins}
-{stats.pole_percentage}
-{stats.pole_starts}
-{stats.points}
+let driverStats = {
+    driver: driver.name,
+    wins: driver.wins,
+    pole_percentage: driver.pole_percentage,
+    points: driver.points
+};
 
-]
+localStorage.setItem("driverStats", JSON.stringify(driverStats));
+renderToString();
+
+
+function renderToString() {
+    let driverStats = JSON.parse(localStorage.getItem("driverStats"));
+    if (driverStats !==null) {
+        document.querySelector(".stat-1").textContent = driverStats.driver
+    }
+}
+
+
+
+// stats = [ driver stats]
+// stats = 
+// [
+// {stats.wins}
+// {stats.pole_percentage}
+// {stats.pole_starts}
+// {stats.points}
+
+// ]
 
 
 get.querrySelector.stats_container 
