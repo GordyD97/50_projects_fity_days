be8a71be68f4536e5219f94a1365e4f48162e28b
Gordon D <gordon.draine97@yahoo.com>
2023-11-24 21:31:55 -0500

    adding select menu and new container styles to folders

--- Driver_profile_Cards/script.js
+++ Driver_profile_Cards/script.js
@@ -1,25 +1,77 @@
-let driverStats = {
-    driver: driver.name,
-    wins: driver.wins,
-    pole_percentage: driver.pole_percentage,
-    points: driver.points
-};
-
-localStorage.setItem("driverStats", JSON.stringify(driverStats));
-renderToString();
 
 
-function renderToString() {
-    let driverStats = JSON.parse(localStorage.getItem("driverStats"));
-    if (driverStats !==null) {
-        document.querySelector(".stat-1").textContent = driverStats.driver
+async function fetchData() {
+    try {
+        // Fetch the JSON data
+        const response = await fetch('stats.json');
+
+        // Parse the JSON data
+        const allDriverStats = await response.json();
+
+        // Assuming "driver" is an HTML element, you might want to get its value, not its name
+        let selectedDriver = document.getElementById("driverStats").value;
+
+        // Check if a driver is selected
+        if (selectedDriver) {
+            // Get the selected driver's stats
+            let driverStats = allDriverStats[selectedDriver];
+
+            // Check if stats are available
+            if (driverStats) {
+                // Store the selected driver's stats in localStorage
+                localStorage.setItem("driverStats", JSON.stringify(driverStats));
+
+                // Render the stats on the page
+                renderStats(driverStats);
+            } else {
+                console.error("Driver stats not available");
+            }
+        } else {
+            console.error("No driver selected");
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
     }
 }
 
+function renderStats(driverStats) {
+    // Assuming you have elements with the class "stat-1", "stat-2", etc.
+    document.querySelector(".stat-1").textContent = `Driver: ${driverStats.name}`;
+    document.querySelector(".stat-2").textContent = `Wins: ${driverStats.wins}`;
+    document.querySelector(".stat-3").textContent = `Pole Percentage: ${driverStats.pole_percentage}%`;
+    document.querySelector(".stat-4").textContent = `Points: ${driverStats.points}`;
+}
 
+// Call the fetchData function when your script loads
+fetchData();
+
+
+
+
+//  let driver = document.getElementById("driverStats");
+
+// let driverStats = {
+//     driver: driver.name,
+//     wins: driver.wins,
+//     pole_percentage: driver.pole_percentage,
+//     points: driver.points
+// };
+
+// localStorage.setItem("driverStats", JSON.stringify(driverStats));
+// renderToString();
+
+
+// function renderToString() {
+//     let driverStats = JSON.parse(localStorage.getItem("driverStats"));
+//     if (driverStats !==null) {
+//         document.querySelector(".stat-1").textContent = driverStats.driver
+//     }
+// }
+
+// console.log("driverStats");
 
 // stats = [ driver stats]
-// stats = 
+// stats =
 // [
 // {stats.wins}
 // {stats.pole_percentage}
@@ -29,21 +81,20 @@ function renderToString() {
 // ]
 
 
-get.querrySelector.stats_container 
 
 
-function getDriverStats() => {
-    var stats = JSON.parse(localStorage.getItem("driver.stats"));
-    if ({driver.name === driver.name}) {
-        document.querrySelector("stats.wins").textContent = name.wins
-    }
-    else ({driver.name === driver.name}) {
-        document.querrySelector("stats.pole_percentage").textContent = name.wins
-    }
-    if ({driver.name === driver.name}) {
-        document.querrySelector("stats.pole_starts").textContent = name.wins
-    }
-    else ({driver.name === driver.name}) {
-        document.querrySelector("stats.points").textContent = name.wins
-    }
-}
+// function getDriverStats() => {
+//     var stats = JSON.parse(localStorage.getItem("driver.stats"));
+//     if ({driver.name === driver.name}) {
+//         document.querrySelector("stats.wins").textContent = name.wins
+//     }
+//     else ({driver.name === driver.name}) {
+//         document.querrySelector("stats.pole_percentage").textContent = name.wins
+//     }
+//     if ({driver.name === driver.name}) {
+//         document.querrySelector("stats.pole_starts").textContent = name.wins
+//     }
+//     else ({driver.name === driver.name}) {
+//         document.querrySelector("stats.points").textContent = name.wins
+//     }
+// }
